45fc21ec65f4b7b92aa7d6f42293fa3393e13951
David Eisinger <david.eisinger@viget.com>
2009-07-18 11:46:27 -0400

    First commit

---
+++ tadbird.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'json'
+
+# If you say "peekaboo" to him, he replies back "thppp".
+
+# If you @reply him three times, he adds you as a friend.
+
+# Then when you say "peekaboo", he says "peekaboo".
+
+# Once you're friends, tag anythign #tadbird and he'll retweet it.
+
+data = JSON.parse(File.open("tad.json").read)
+
+raise data .inspect
